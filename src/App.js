d7201420398e2067d96406726c5ecef64369e3ea
Abico Dev <abenezerendalu777@gmail.com>
2026-06-17 00:17:50 +0300

    first step

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import "./App.css";
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Netflix Clone</h1>
+      <p>Welcome to your Netflix Clone</p>
+    </div>
+  );
+}
+
+export default App;
