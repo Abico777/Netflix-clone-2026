43fb61772c9f0b5e1471b794fb15093a3d2ab599
Abico Dev <abenezerendalu777@gmail.com>
2026-06-17 05:01:14 +0300

    let go

--- src/App.js
+++ src/App.js
@@ -1,12 +1,11 @@
 import "./App.css";
 
 function App() {
-  return (
-    <div className="App">
-      <h1>Netflix Clone</h1>
-      <p>Welcome to your Netflix Clone</p>
-    </div>
-  );
+    return (
+        <div className="App">
+            <h1>Netflix Clone & yeah</h1>
+        </div>
+    );
 }
 
 export default App;
